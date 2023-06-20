d65c542fc4a123a1ff12955874206e12add9976b
JessicaAndrango <93225476+JessicaAndrango@users.noreply.github.com>
2023-06-20 16:18:56 -0500

    stencil

---
+++ webtraining/src/components/my-component/web-training-personal/web-training-personal.tsx
@@ -0,0 +1,14 @@
+import {Component, h} from '@stencil/core';
+
+@Component({
+    tag: 'web-training-personal'
+})
+export class Webtrainingpersonal{
+    render(){
+        return(
+        <div>
+            <h1>Soy un componente creado con la libreria stencil ♥ </h1>
+            </div>
+        );
+    }
+}
